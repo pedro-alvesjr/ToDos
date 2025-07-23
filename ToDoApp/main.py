82a22ecbec922c63f2ad0f4882f3826bc113d850
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-07-23 12:12:22 -0300

    main.py update

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 import models
-from database import engine
+from database import engine, SessionLocal
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal
+    try:
+        yield db
+    finally:
+        db.close()
