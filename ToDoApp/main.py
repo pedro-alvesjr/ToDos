ea050a081317339aa103cc9bbbd16f3485f9b895
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-07-24 10:57:32 -0300

    update main.py
    read by ID endpoint added

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 import models
 from sqlalchemy.orm import Session
 from models import Todos
@@ -23,3 +23,11 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.get("/")
 def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}")
+def read_todo(db: db_dependency, todo_id: int):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model:
+        return todo_model
+    raise HTTPException(status_code=404, detail='ID not found')
