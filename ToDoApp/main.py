c46630b302c0aa8f0e7304eb1385e2fe93cc45f7
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-07-24 00:12:38 -0300

    update main.py

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,15 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
+from sqlalchemy.orm import Session
+from models import Todos
 from database import engine, SessionLocal
+from typing import Annotated
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+
 def get_db():
-    db = SessionLocal
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+@app.get('/')
+def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()
