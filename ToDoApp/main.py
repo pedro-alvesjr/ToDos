25431a7f2bfbc39cbb83d37fd138c50b249825d2
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-07-24 11:03:55 -0300

    update main.py
    added Path validation + status_code 200

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status, Path
 import models
 from sqlalchemy.orm import Session
 from models import Todos
@@ -20,13 +20,13 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def read_all(db: db_dependency):
     return db.query(Todos).all()
 
 
-@app.get("/todo/{todo_id}")
-def read_todo(db: db_dependency, todo_id: int):
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model:
         return todo_model
