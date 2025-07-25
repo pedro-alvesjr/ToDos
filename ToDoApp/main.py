051cc3e43f6eab790aecf776352bc04c8fc48a9d
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-07-24 23:55:41 -0300

    update main.py
    added update_todo

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -35,7 +35,8 @@ def read_all(db: db_dependency):
 
 
 @app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
-def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+def read_todo(db: db_dependency, 
+              todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model:
         return todo_model
@@ -43,7 +44,24 @@ def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
-def create_todo(db: db_dependency, todo_request: TodoRequest):
+def create_todo(db: db_dependency, 
+                todo_request: TodoRequest):
     todo_model = Todos(**todo_request.model_dump())
     db.add(todo_model)
+    db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+def update_todo(db: db_dependency, 
+                todo_request: TodoRequest,
+                todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='ID not found.')
+    
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
     db.commit()
