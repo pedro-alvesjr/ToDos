7dbee2e47bf38ed7ccdcaa87c9cf7b49cbaebdda
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-07-25 23:00:53 -0300

    Update Auth.py
    Created CreateUserRequest class

--- ToDoApp/routers/auth.py
+++ ToDoApp/routers/auth.py
@@ -1,7 +1,30 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
+
 
 router = APIRouter()
 
-@router.get("/auth/")
-def get_user():
-    return {'user': 'authenticated'}
+
+class CreateUserRequest(BaseModel):
+    email: str
+    username: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/auth")
+def create_user(create_user_request: CreateUserRequest):
+    create_user_request = Users(
+        email = create_user_request.email,
+        username = create_user_request.username,
+        first_name = create_user_request.first_name,
+        last_name = create_user_request.last_name,
+        hashed_password = create_user_request.password,
+        role = create_user_request.role,
+        is_active = True
+    )
+
+    return create_user_request
