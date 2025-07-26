b546e784fc2457364ec68145f3ef72b816df21a4
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-07-25 21:20:43 -0300

    update models.py
    created Users table/object

--- ToDoApp/models.py
+++ ToDoApp/models.py
@@ -1,6 +1,21 @@
 from sqlalchemy import Integer, Boolean, String, Column
 from database import Base
 
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
+
+
 class Todos(Base):
     __tablename__ = 'todos'
 
