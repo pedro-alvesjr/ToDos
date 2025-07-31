111bc3acae2a10574196e80dfb4ff83af8bbda85
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-07-30 23:27:05 -0300

    Update auth.py
    class Token debugged

--- ToDoApp/routers/auth.py
+++ ToDoApp/routers/auth.py
@@ -52,7 +52,7 @@ class CreateUserRequest(BaseModel):
 
 
 class Token(BaseModel):
-    acess_token: str
+    access_token: str
     token_type: str
 
 
@@ -72,11 +72,12 @@ def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     db.commit()
 
 
-@router.post("/token")
+@router.post("/token", response_model = Token)
 def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                            db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return 'Failed authentication.'
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
-    return token
+    
+    return {'access_token': token, 'token_type': 'bearer'}
