c70081590f3a839264aac657675ad246d1155ad9
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-08-05 15:49:27 -0300

    Update auth.py

--- ToDoApp/routers/auth.py
+++ ToDoApp/routers/auth.py
@@ -92,7 +92,8 @@ def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depen
                            db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        return 'Failed authentication.'
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                detail='Could not validate user.')
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
     
     return {'access_token': token, 'token_type': 'bearer'}
