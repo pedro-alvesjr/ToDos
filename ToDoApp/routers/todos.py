f6d29307800a05be929811496f48581c3995dc94
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-08-07 22:07:49 -0300

    Update todos.py
    Added user_dependency to read_all function.

--- ToDoApp/routers/todos.py
+++ ToDoApp/routers/todos.py
@@ -28,8 +28,8 @@ class TodoRequest(BaseModel):
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-def read_all(db: db_dependency):
-    return db.query(Todos).all()
+def read_all(user: user_dependency, db: db_dependency):
+    return db.query(Todos).filter(Todos.owner_id == user.get('id')).all()
 
 
 @router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
