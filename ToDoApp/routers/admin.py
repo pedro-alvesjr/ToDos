0a37e56fab70b275c6568a98e2a51f4acd321117
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-08-09 11:26:00 -0300

    Created admin.py

---
+++ ToDoApp/routers/admin.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends, HTTPException, status, Path
+from sqlalchemy.orm import Session
+from models import Todos
+from database import SessionLocal
+from typing import Annotated
+from pydantic import BaseModel, Field
+from .auth import get_current_user
+
+router = APIRouter(
+    prefix='/admin',
+    tags=['admin']
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+@router.get('/todos', status_code=status.HTTP_200_OK)
+def read_all(user: user_dependency, db: db_dependency):
+    if user is None or user.get('user_role') != 'admin':
+        return HTTPException(status_code=401, detail='User not authenticated.')
+    
+    return db.query(Todos).all
--- ToDoApp/routers/auth.py
+++ ToDoApp/routers/auth.py
@@ -41,9 +41,9 @@ def authenticate_user(username: str, password: str, db):
         return False
     return user
 
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+def create_access_token(username: str, user_id: int, user_role: str, expires_delta: timedelta):
     expires = datetime.now(UTC) + expires_delta
-    encode = {'sub': username, 'id': user_id, 'exp': expires}
+    encode = {'sub': username, 'id': user_id, 'role': user_role, 'exp': expires}
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
@@ -51,10 +51,13 @@ def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
+        user_role: str = payload.get('role')
+
         if username is None or user_id is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                                 detail='Could not validate user.')
-        return {'username': username, 'id': user_id}
+        return {'username': username, 'id': user_id, 'role': user_role}
+    
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                             detail='Could not validade user.')
@@ -97,6 +100,6 @@ def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depen
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                                 detail='Could not validate user.')
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
     
     return {'access_token': token, 'token_type': 'bearer'}
