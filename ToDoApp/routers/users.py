d4491a07a5fb517fb046de1c23d53a4e2c926851
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-08-13 23:36:21 -0300

    Update users.py

--- ToDoApp/routers/users.py
+++ ToDoApp/routers/users.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Path
 from pydantic import BaseModel
 from database import SessionLocal
 from models import Users
@@ -24,9 +24,19 @@ def get_db():
     finally:
         db.close()
 
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto') 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+
 
 @router.get('/user')
 def get_user(db: db_dependency, user: user_dependency):
     return db.query(Users).filter(Users.id == user.get('id')).first()
+
+@router.put('/users/{user_id}')
+def change_password(db: db_dependency, 
+                    user: user_dependency, 
+                    change_password_request: str = Path(min_length=5)):
+    user.get('hashed_password') = bcrypt_context.hash(change_password_request)
+
